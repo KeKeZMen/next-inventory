70a15eccc7d20e028ff79fe293d9b4a2fc2c6d90
Kekez <makeskex@yandex.ru>
2023-11-21 16:31:38 +0300

    key prop for cabinet page

--- src/app/cabinet/page.tsx
+++ src/app/cabinet/page.tsx
@@ -74,7 +74,12 @@ export default async function CabinetPage({
       <Header isNoAdmin user={session.user} />
       <main className="pt-[88px] md:p-6">
         {productsWithCabinets.map((cabinet) => (
-          <ProductsTable title={`Позиции ${cabinet.name} кабинета`} withoutEdit margin="0 0 15px 0">
+          <ProductsTable
+            title={`Позиции ${cabinet.name} кабинета`}
+            withoutEdit
+            margin="0 0 15px 0"
+            key={cabinet.id}
+          >
             {cabinet.products.map((product) => (
               <ProductRow product={product} key={product.id} withoutEdit />
             ))}
